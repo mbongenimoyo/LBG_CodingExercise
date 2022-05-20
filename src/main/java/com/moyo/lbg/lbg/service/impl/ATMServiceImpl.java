3dff6c23541a9d4413f37aeb0d5711f8eb89de94
mbong <m.bong3@hotmail.com>
2022-05-20 13:55:12 +0100

    dockerfile now exposes port 8080

--- src/main/java/com/moyo/lbg/lbg/service/impl/ATMServiceImpl.java
+++ src/main/java/com/moyo/lbg/lbg/service/impl/ATMServiceImpl.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
-@TrackExecutionTime
 public class ATMServiceImpl implements ATMService {
     private final Logger Logger= LogManager.getLogger(ATMServiceImpl.class);
     @Autowired
